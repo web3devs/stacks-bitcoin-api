eec87c2a5f46b920576416b7e3e7db852e32fbae
bgok <ken@citizenhodler.com>
2022-06-15 09:21:55 -0700

    Create BlockApiClient and ClarityBitcoinClient

---
+++ src/ClarityBitcoinClient.ts
@@ -0,0 +1,43 @@
+import {bufferCV, callReadOnlyFunction, ClarityValue, cvToValue, uintCV} from "@stacks/transactions";
+
+const {
+    CLARITY_BITCOIN_CONTRACT_NAME,
+    CLARITY_BITCOIN_CONTRACT_ADDRESS,
+    NETWORK,
+    SENDER_ADDRESS
+} = process.env
+
+export const verifyBlockHeader = async (blockHeader: Buffer, stxBlockHeight: number): Promise<boolean> => {
+    // (verify-block-header (headerbuff (buff 80)) (expected-block-height uint))
+    const result = await callReadOnlyFunction({
+        contractName: CLARITY_BITCOIN_CONTRACT_NAME as string,
+        contractAddress: CLARITY_BITCOIN_CONTRACT_ADDRESS as string,
+        functionName: 'verify-block-header',
+        functionArgs: [
+            bufferCV(blockHeader),
+            uintCV(stxBlockHeight)
+        ],
+        network: NETWORK as any,
+        senderAddress: SENDER_ADDRESS as string,
+    })
+    return cvToValue(result)
+}
+//
+// export const getReversedTxId = async (tx: Buffer): Promise<any> => {
+//     // (define-read-only (get-reversed-txid (tx (buff 1024)))
+//     const functionName = 'get-reversed-txid'
+//     const functionArgs: ClarityValue[] = [
+//         bufferCV(tx)
+//     ]
+//
+//     const result = await callReadOnlyFunction({
+//         contractName: CLARITY_BITCOIN_CONTRACT_NAME as string,
+//         contractAddress: CLARITY_BITCOIN_CONTRACT_ADDRESS as string,
+//         functionName,
+//         functionArgs,
+//         network: NETWORK as any,
+//         senderAddress: SENDER_ADDRESS as string,
+//     })
+//     return cvToValue(result)
+// }
+//
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
 import {getBlock, getBlockHeader, getRawTransaction} from "./rpcclient.js" // TODO Why is the .js extension required?
 import {
     BufferCV,
+    bufferCV,
     callReadOnlyFunction,
+    ClarityValue,
+    cvToValue,
     listCV,
     tupleCV,
     uintCV,
-    ClarityValue,
-    bufferCV,
-    cvToValue,
 } from "@stacks/transactions"
 import 'dotenv/config'
 import {MerkleTree} from "merkletreejs"
 import SHA256 from "crypto-js/sha256.js"
-import {BlocksApi, Configuration} from '@stacks/blockchain-api-client'
+import {verifyBlockHeader} from "./ClarityBitcoinClient.js"
+import {getStxBlockHeight} from "./BlockApiClient.js"
 
-const {CLARITY_BITCOIN_CONTRACT_NAME, CLARITY_BITCOIN_CONTRACT_ADDRESS} = process.env
-
-const config = new Configuration({basePath: 'https://stacks-node-api.testnet.stacks.co'})
-const blocksApi = new BlocksApi(config)
+const {
+    CLARITY_BITCOIN_CONTRACT_NAME,
+    CLARITY_BITCOIN_CONTRACT_ADDRESS,
+    NETWORK,
+    SENDER_ADDRESS
+} = process.env
 
 interface ProvableTx {
     tx: Buffer,
@@ -61,28 +64,6 @@ const getTxProof = async (txId: string): Promise<ProvableTx> => {
 }
 
 
-const verifyBlockHeader = async ({blockHeader, stxBlockHeight}: ProvableTx) :Promise<any> => {
-    // (verify-block-header (get header block) (get height block)
-    const functionName = 'verify-block-header'
-    const functionArgs: ClarityValue[] = [
-        bufferCV(blockHeader),
-        uintCV(stxBlockHeight)
-    ]
-    const network = 'testnet'
-    const senderAddress = 'ST24YYAWQ4DK4RKCKK1RP4PX0X5SCSXTWQXFGVCVY'
-
-    const result = await callReadOnlyFunction({
-        contractName: CLARITY_BITCOIN_CONTRACT_NAME as string,
-        contractAddress: CLARITY_BITCOIN_CONTRACT_ADDRESS as string,
-        functionName,
-        functionArgs,
-        network,
-        senderAddress,
-    })
-    return cvToValue(result)
-
-}
-
 const getBlockHeaderHash = async (blockHeight: number): Promise<any> => {
     // (get-bc-h-hash (bh uint))
     const functionName = 'get-bc-h-hash'
@@ -90,16 +71,13 @@ const getBlockHeaderHash = async (blockHeight: number): Promise<any> => {
         uintCV(58225)
     ]
 
-    const network = 'testnet'
-    const senderAddress = 'ST24YYAWQ4DK4RKCKK1RP4PX0X5SCSXTWQXFGVCVY'
-
     const result = await callReadOnlyFunction({
         contractName: CLARITY_BITCOIN_CONTRACT_NAME as string,
         contractAddress: CLARITY_BITCOIN_CONTRACT_ADDRESS as string,
         functionName,
         functionArgs,
-        network,
-        senderAddress,
+        network: NETWORK as any,
+        senderAddress: SENDER_ADDRESS as string,
     })
     return cvToValue(result)
 
@@ -112,16 +90,13 @@ const getReversedTxId = async ({tx, txId}: ProvableTx): Promise<any> => {
         bufferCV(tx)
     ]
 
-    const network = 'testnet'
-    const senderAddress = 'ST24YYAWQ4DK4RKCKK1RP4PX0X5SCSXTWQXFGVCVY'
-
     const result = await callReadOnlyFunction({
         contractName: CLARITY_BITCOIN_CONTRACT_NAME as string,
         contractAddress: CLARITY_BITCOIN_CONTRACT_ADDRESS as string,
         functionName,
         functionArgs,
-        network,
-        senderAddress,
+        network: NETWORK as any,
+        senderAddress: SENDER_ADDRESS as string,
     })
     console.assert(reverseBuffer(Buffer.from(txId, 'hex')).toString('hex') === cvToValue(result), txId)
     return cvToValue(result)
@@ -146,41 +121,22 @@ const verifyProofOnStacks = async ({ stxBlockHeight, blockHeader, tx, txIndex, p
             "tree-depth": uintCV(proof.length)
         })
     ]
-    const network = 'testnet'
-    const senderAddress = 'ST24YYAWQ4DK4RKCKK1RP4PX0X5SCSXTWQXFGVCVY'
-
     const result = await callReadOnlyFunction({
         contractName: CLARITY_BITCOIN_CONTRACT_NAME as string,
         contractAddress: CLARITY_BITCOIN_CONTRACT_ADDRESS as string,
         functionName,
         functionArgs,
-        network,
-        senderAddress,
+        network: NETWORK as any,
+        senderAddress: SENDER_ADDRESS as string,
     })
     return cvToValue(result).value
 }
 
-async function getStxBlockHeight(bitcoinBlockHeight: number): Promise<number | undefined> {
-    let limit = 30;
-    let offset = 0;
-    const firstResponse = await blocksApi.getBlockList({ offset, limit });
-    let stxBlock = firstResponse.results.find(b => b.burn_block_height === bitcoinBlockHeight);
-    offset += Math.max(limit, firstResponse.results[0].burn_block_height - bitcoinBlockHeight);
-    while (!stxBlock) {
-        const blockListResponse = await blocksApi.getBlockList({ offset, limit });
-        const blocks = blockListResponse.results;
-        stxBlock = blocks.find(b => b.burn_block_height === bitcoinBlockHeight);
-        offset -= limit;
-        if (offset < 0 || blocks[blocks.length - 1].burn_block_height > bitcoinBlockHeight)
-            return undefined;
-    }
-    return stxBlock.height;
-}
-
 getTxProof(txid)
     // .then(getBlockHeaderHash)
-    // .then(verifyBlockHeader)
-    .then(verifyProofOnStacks)
+    .then(({blockHeader, stxBlockHeight}: ProvableTx): Promise<boolean> =>
+        verifyBlockHeader(blockHeader, stxBlockHeight))
+    // .then(verifyProofOnStacks)
     // .then(getReversedTxId)
     .then(console.log)
     .catch(console.error)
